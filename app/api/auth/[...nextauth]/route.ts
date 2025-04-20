93e1b9f4999c97d60022e681604fdf4c4314ace9
megaVE <viniherofactory@gmail.com>
2025-04-20 14:58:32 -0300

    build error fix

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,19 +1,19 @@
 import { handlers } from "@/app/lib/auth";
-import NextAuth from "next-auth";
-import Google from "next-auth/providers/google";
+// import NextAuth from "next-auth";
+// import Google from "next-auth/providers/google";
 
 export const { GET, POST } = handlers;
 
-export const { auth, signOut } = NextAuth({
-	providers: [
-		Google({
-			authorization: {
-				params: {
-					prompt: "consent",
-					access_type: "offline",
-					response_type: "code",
-				},
-			},
-		}),
-	],
-});
+// export const { auth, signIn, signOut } = NextAuth({
+// 	providers: [
+// 		Google({
+// 			authorization: {
+// 				params: {
+// 					prompt: "consent",
+// 					access_type: "offline",
+// 					response_type: "code",
+// 				},
+// 			},
+// 		}),
+// 	],
+// });
